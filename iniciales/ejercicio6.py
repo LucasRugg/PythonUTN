09532d8e0823fd3d8442470387ff29cad3d4a035
Lucas Ruggiero <lucas.ruggiero001@gmail.com>
2024-04-08 08:17:13 -0300

    Ordeno archivos

--- iniciales/ejercicio6.py
+++ iniciales/ejercicio6.py
@@ -6,4 +6,4 @@ costeTotal = precio * unidades
 
 # Formateo de salida usando cadenas de formato
 # {:10.2f} indica un número con un total de 10 caracteres (incluyendo el punto decimal y los decimales) y 2 decimales
-print("\nNombre del producto: {}\nPrecio unitario: {:10.2f}\nNúmero de unidades: {:3}\nCosto total del producto: {:10.2f}".format(producto.upper(), precio, unidades, costeTotal))
+print("Nombre del producto: {}Precio unitario: {:10.2f} Número de unidades: {:3}Costo total del producto: {:10.2f}".format(producto, precio, unidades, costeTotal))
